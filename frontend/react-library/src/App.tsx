cb29c416597e8418f6d6df4f224cf541e32d0e96
Thanh Duy <duythanh19111999@gmail.com>
2025-04-17 16:58:34 +0700

    create homepage

---
+++ frontend/react-library/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
+
+function App() {
+  return (
+    <div>
+      {/* Navigation bar component */}
+      <Navbar />
+      {/* HomePage */}
+      <ExploreTopBooks />
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
