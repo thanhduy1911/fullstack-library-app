324d639e9cfd006649a765128bea702abbe37e04
Thanh Duy <duythanh19111999@gmail.com>
2025-04-17 19:24:15 +0700

    cleanup homepage

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/HomePage/Carousel';
+import { ExploreTopBooks } from './layouts/HomePage/components/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/components/Carousel';
+import { Heros } from './layouts/HomePage/components/Heros';
+import { LibraryServices } from './layouts/HomePage/components/LibraryServices';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
 
-function App() {
+export const App = () => {
   return (
     <div>
-      {/* Navigation bar component */}
+      {/* Navigation bar */}
       <Navbar />
+
       {/* HomePage */}
-      <ExploreTopBooks />
-      <Carousel />
+      <HomePage />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
-
-export default App;
