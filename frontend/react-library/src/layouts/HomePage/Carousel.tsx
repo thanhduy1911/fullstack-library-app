315840f3e2d1ca468e35d6012435e6807c191c99
Thanh Duy <duythanh19111999@gmail.com>
2025-04-17 18:47:02 +0700

    fix carousel for mobile display

--- frontend/react-library/src/layouts/HomePage/Carousel.tsx
+++ frontend/react-library/src/layouts/HomePage/Carousel.tsx
@@ -54,45 +54,45 @@ export const Carousel = () => {
                                     />
                                     <h6 className="mt-2">Book</h6>
                                     <p>Love Reading</p>
-                                    <a className="btn main-color text-white" href="#">Reserved</a>
+                                    <a className="btn main-color text-white" href="#">Reserve</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <button className="carousel-control-prev" type="button"
-                        data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button"
-                        data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
                 </div>
-                {/* Mobile */}
-                <div className="d-lg-none mt-3">
-                    <div className="row d-flex justify-content-centre align-items-center">
-                        <div className="text-center">
-                            <img
-                                src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                                width={151}
-                                height={233}
-                                alt="book"
-                            />
-                            <h6 className="mt-2">
-                                <b>Book</b>
-                            </h6>
-                            <p>
-                                Love Coding
-                            </p>
-                            <a className="btn main-color text-white" href="#">Reserved</a>
-                        </div>
+                <button className="carousel-control-prev" type="button"
+                    data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button"
+                    data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+            {/* Mobile */}
+            <div className="d-lg-none mt-3">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="text-center">
+                        <img
+                            src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                            width='151'
+                            height='233'
+                            alt="book"
+                        />
+                        <h6 className="mt-2">
+                            <b>Book</b>
+                        </h6>
+                        <p>
+                            Love Coding
+                        </p>
+                        <a className="btn main-color text-white" href="#">Reserve</a>
                     </div>
                 </div>
-                <div className="homepage-carousel-title mt-3">
-                    <a className="btn btn-outline-secondary btn-lg" href="#">View More</a>
-                </div>
+            </div>
+            <div className="homepage-carousel-title mt-3">
+                <a className="btn btn-outline-secondary btn-lg" href="#">View More</a>
             </div>
         </div>
     );
