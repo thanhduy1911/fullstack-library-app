6c0aa04d2c4c6ba0fd2698696ca7312e5b80915e
Thanh Duy <duythanh19111999@gmail.com>
2025-04-19 00:05:46 +0700

    add signup function using okta service

---
+++ frontend/react-library/src/Auth/LoginWidget.jsx
@@ -0,0 +1,32 @@
+import { Redirect } from 'react-router-dom';
+import { useOktaAuth } from '@okta/okta-react';
+import { SpinnerLoading } from '../layouts/Utils/SpinnerLoading';
+import OktaSignInWidget from './OktaSigninWidget';
+
+const LoginWidget = ({ config, originalUri = '/' }) => {
+    const { oktaAuth, authState } = useOktaAuth();
+
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens, originalUri);
+    };
+
+    const onError = (err) => {
+        console.log('Sign in error:', err);
+    };
+
+    if (!authState) {
+        return <SpinnerLoading />;
+    }
+
+    return authState.isAuthenticated ? (
+        <Redirect to={originalUri} />
+    ) : (
+        <OktaSignInWidget
+            config={config}
+            onSuccess={onSuccess}
+            onError={onError}
+        />
+    );
+};
+
+export default LoginWidget;
--- frontend/react-library/src/lib/oktaConfig.ts
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -1,8 +1,9 @@
 export const oktaConfig = {
-    clientId: '0oaocig7zkxRXc0nR5d7',
-    issuer: 'https://dev-58558560.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    clientId: `0oaocig7zkxRXc0nR5d7`,
+    issuer: `https://dev-58558560.okta.com/oauth2/default`,
+    redirectUri: `http://localhost:3000/login/callback`,
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
+    useClassicEngine: true
 }
