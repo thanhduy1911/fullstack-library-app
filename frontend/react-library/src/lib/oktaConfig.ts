819498c5c8c77f3a367748cd9eea2d09690ed9a4
Thanh Duy <duythanh19111999@gmail.com>
2025-04-18 21:58:42 +0700

    add okta configuration and install dependencies

---
+++ frontend/react-library/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oaocig7zkxRXc0nR5d7',
+    issuer: 'https://dev-58558560.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
