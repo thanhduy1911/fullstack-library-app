06c8a90b03dd25007687828a0b55a0365f30c412
Thanh Duy <duythanh19111999@gmail.com>
2025-04-20 18:31:07 +0700

    add admin manged function

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -12,6 +12,7 @@ import LoginWidget from "./Auth/LoginWidget";
 import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/ReviewListPage";
 import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
 import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
+import { ManageLibraryPage } from "./layouts/ManageLibraryPage/ManageLibraryPage";
 
 export const App = () => {
   const history = useHistory();
@@ -60,6 +61,9 @@ export const App = () => {
             </SecureRoute>
             <SecureRoute path="/messages">
               <MessagesPage />
+            </SecureRoute>
+            <SecureRoute path="/admin">
+              <ManageLibraryPage />
             </SecureRoute>
           </Switch>
         </div>
---
+++ frontend/react-library/src/layouts/ManageLibraryPage/components/AdminMesages.tsx
@@ -0,0 +1,88 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { useEffect, useState } from "react";
+import MessageModel from "../../../models/MessageModel";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+import { Pagination } from "../../Utils/Pagination";
+
+export const AdminMessages = () => {
+  const { authState } = useOktaAuth();
+  // Normal Loading Pieces
+  const [isLoadingMessages, setIsLoadingMessages] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  // Messages endpoint State
+  const [messages, setMessages] = useState<MessageModel[]>([]);
+  const [messagesPerPage] = useState(5);
+
+  // Pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    const fetchUserMessages = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/messages/search/findByClosed?closed=false&page=${
+          currentPage - 1
+        }&size=${messagesPerPage}`;
+        const token = authState.accessToken?.accessToken;
+        const requestOptions = {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const messagesResponse = await fetch(url, requestOptions);
+        if (!messagesResponse.ok) {
+          throw new Error("Something went wrong while getting messages");
+        }
+        const responseJson = await messagesResponse.json();
+
+        setMessages(responseJson._embedded.messages);
+        setTotalPages(responseJson.page.totalPages);
+      }
+      setIsLoadingMessages(false);
+    };
+    fetchUserMessages().catch((error: any) => {
+      setIsLoadingMessages(false);
+      setHttpError(error.message);
+    });
+    window.scrollTo(0, 0);
+  }, [authState, currentPage]);
+
+  if (isLoadingMessages) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="mt-3">
+      {messages.length > 0 ? (
+        <>
+          <h5>Pending Q/A: </h5>
+          {messages.map((message) => (
+            <p>Questions that need a response</p>
+          ))}
+        </>
+      ) : (
+        <h5>No pending Q/A</h5>
+      )}
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          paginate={paginate}
+        />
+      )}
+    </div>
+  );
+};
